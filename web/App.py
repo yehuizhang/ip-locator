8065da35796536ff53cfce2932474d629e6609fc
Yehui <yehuizhang@outlook.com>
2019-09-22 04:08:58 -0700

    test run on Windows, and fixed some issues

--- web/App.py
+++ web/App.py
@@ -2,7 +2,7 @@
 # https://flask-restful.readthedocs.io/en/latest/quickstart.html#a-minimal-api
 from flask import Flask
 from flask_restful import Resource, Api
-#from resources.Greeting import Greeting
+from resources.Greeting import Greeting
 
 app = Flask(__name__)
 api = Api(app)
@@ -12,7 +12,7 @@ class HelloWorld(Resource):
         return {'hello': 'world'}
 
 api.add_resource(HelloWorld, '/')
-print("Hello")
+api.add_resource(Greeting, '/greeting')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
--- web/resources/Greeting.py
+++ web/resources/Greeting.py
@@ -1,3 +1,5 @@
+from flask_restful import Resource
+
 class Greeting(Resource):
     def get(self):
         return {'message': 'Greetings from Yehui.'}
